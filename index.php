60b5b0945f0524f869c76bb25bafda1fa4c4d9f7
Junny Noriega <junnynoriega@gmail.com>
2023-02-09 06:15:46 -0500

    updated files

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -13,7 +12,7 @@ require_once('vendor/autoload.php');
 // instantiate F3 base class
 $f3 = Base::instance();
 
-// define a default route (328/diner)
+// define a default route (328/home)
 $f3->route('GET /', function () {
 
     // instantiate a view
@@ -21,7 +20,40 @@ $f3->route('GET /', function () {
     echo $view->render("views/home.html");
 });
 
+// Define a default route (328/home) ---> this one is used to go back to homepage
+$f3->route('GET|POST /home', function () {
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
 
+// Define a default route (328/about)
+$f3->route('GET|POST /about', function () {
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render('views/about.html');
+});
+
+// Define a default route (328/contact)
+$f3->route('GET|POST /contact', function () {
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render('views/contact.html');
+});
+
+// Define a default route (328/sign-in)
+$f3->route('GET|POST /signin', function () {
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render('views/sign-in.html');
+});
+
+// Define a default route (328/sign-up)
+$f3->route('GET|POST /signup', function () {
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render('views/sign-up.html');
+});
 
 // run Fat Free
 $f3->run();
