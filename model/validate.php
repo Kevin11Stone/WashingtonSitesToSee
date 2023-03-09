9e462c3f14ae525714d6776b7a73afd2427e44e7
Junny Noriega <junnynoriega@gmail.com>
2023-03-09 04:04:36 -0500

    updated file

--- model/validate.php
+++ model/validate.php
@@ -15,25 +15,7 @@ class Validate
     }
 
     static function validPassword($password) {
-        if(!empty($password)) {
-            if (strlen($password) <= 8) {
-                return "Your Password Must Contain At Least 8 Characters!";
-            }
-            elseif(!preg_match("#[0-9]+#",$password)) {
-                return "Your Password Must Contain At Least 1 Number!";
-            }
-            elseif(!preg_match("#[A-Z]+#",$password)) {
-                return "Your Password Must Contain At Least 1 Capital Letter!";
-            }
-            elseif(!preg_match("#[a-z]+#",$password)) {
-                return "Your Password Must Contain At Least 1 Lowercase Letter!";
-            }
-            elseif(!preg_match("#[\W]+#",$password)) {
-                return "Your Password Must Contain At Least 1 Special Character!";
-            }
-        } else {
-            return "Please enter password";
-        }
+        return preg_match("/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/",$password);
     }
 
 
